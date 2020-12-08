4471f52cdd252cb53e7afda4fc8081640319ee0d
Rebecca Miller <beemills@gmail.com>
2020-12-08 00:17:11 -0500

    colorOption toggle button complete

--- frontend-challenge/src/Components/OrderingInfo.js
+++ frontend-challenge/src/Components/OrderingInfo.js
@@ -3,17 +3,20 @@ import SizeSelector from './SizeSelector';
 import '../styles/TopSection.css'
 import { useState, useEffect, useRef } from 'react';
 
-const OrderingInfo = () => {
+const OrderingInfo = (props) => {
+
 	const [qty, setQty] = useState(1);
 	const [color, setColor] = useState("black");
 	const colorBlack = useRef(null);
 	const colorBeige = useRef(null);
 
+	//functions for changing quantity counter
 	const increment = () => setQty(prevQty => prevQty + 1)
 	const decrement = () => {
 		if (qty > 1) setQty(prevQty => prevQty - 1);
 	};
 	
+	//functions for changing state of color selected and updating UI:
 	const toggleColor = () => {
 		if (color === 'black' && colorBeige.current) {
 			colorBeige.current.clicked = true;
@@ -26,19 +29,20 @@ const OrderingInfo = () => {
 			setColor("black");
 		}
 	}
+	const char = "\u2714";
 
 	return (
 		<div className="orderingInfo">
 			<div id="price">$35.00</div>
 			<div className="btnGroup">
-					<button className="colorOption">color</button>
-					<button className="colorOption" id="black" ref={colorBlack} onClick={toggleColor}>black</button>
-					<button className="colorOption" id="beige" ref={colorBeige} onClick={toggleColor}>beige</button>
+				<button className="colorOption" style={{outline: 'none'}}>color</button>
+				<button className="colorOption" id="black" ref={colorBlack} onClick={toggleColor}>{color === 'black'? `black ${char}` : 'black' }</button>
+				<button className="colorOption" id="beige" ref={colorBeige} onClick={toggleColor}>{color === 'beige'? `beige ${char}` : 'beige' }</button>
 				</div>
 				<div className="orderQty">
-					<button type="button" onClick={decrement}>-</button>
+					<button onClick={decrement}>-</button>
 					<span id="quantity">{`quantity (${qty})`}</span>
-					<button type="button" onClick={increment}>+</button>
+					<button onClick={increment}>+</button>
 				</div>
 				<SizeSelector title="size" />
 				<button id="cart">add to cart</button>
