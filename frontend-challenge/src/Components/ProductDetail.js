8e13b81ff4a6ea24a3520ee824ce082523dc39db
Rebecca Miller <beemills@gmail.com>
2020-12-08 23:37:26 -0500

    zoom feature added in for first product image

--- frontend-challenge/src/Components/ProductDetail.js
+++ frontend-challenge/src/Components/ProductDetail.js
@@ -1,34 +1,46 @@
-import React from 'react';
-import { One, Two, Three, Four, Five, Six } from './ImageImport';
+import React, { useState, useRef } from 'react';
 import ProductDescription from './ProductDescription';
 import OrderingInfo from './OrderingInfo';
-import ShopGrid from './ShopGrid'
+import ShopGrid from './ShopGrid';
+import { One, Two, Three, Four, Five, Six } from './ImageImport';
 
-import './../styles/TopSection.css'
+import './../styles/TopSection.css';
 
 //react map all files in a folder - webpack alias to import
-const ProductDetail = () => {
+const ProductDetail = (props) => {
 
-	return (
-		<div className="mainPage">
-				<div className="topSection">
-					<ProductDescription />
-					<div className="product-container">
-						<img src={Two} className="product-image" alt="main" />
-						{/* <img src={One} className="product-image" alt="main" />
-						<img src={Three} className="product-image" alt="main" />
-						<img src={Four} className="product-image" alt="main" />
-						<img src={Five} className="product-image" alt="main" />
-						<img src={Six} className="product-image" alt="main" /> */}
+	const imgRef = useRef(null);
+
+	const [zoomed, setZoomed] = useState(false);
+	const [current, setCurrent] = useState(null);
+	
+	const zoom = () => {
+    setZoomed(!zoomed);
+    console.log(imgRef.current)
+  }
+
+
+
+			return (
+					<div>
+						<div className="topSection">
+						<div className="product-container">
+							<img src={Two} className={`product-image${zoomed ? "-zoomed" : ""}`} ref={imgRef} alt="main-product-2" key="2" onClick={zoom}></img>
+							<img src={One} alt="main-product-1" key="1" onClick={zoom}></img>
+							<img src={Three} alt="main-product-3" key="3"></img>
+							<img src={Four} alt="main-product-4" key="4"></img>
+							<img src={Five} alt="main-product-5" key="5"></img>
+							<img src={Two} alt="main-product-6" key="6"></img>
+							{/* <img src={Two} key="productImageOne" className={`product-image${zoomed ? "-zoomed" : ""}`} alt="product-view-1"/> */}
+						</div>
+							{!zoomed && <ProductDescription />}
+							{!zoomed && <OrderingInfo zoomed={zoomed} current={current}/>}
 					</div>
-					<OrderingInfo />
-			</div>
-			<div className="bottomSection">
-				<ShopGrid />
-			</div>	
-		</div>
-		
-	)
-}
+					<div className="bottomSection">
+						{/* {!zoomed && <ShopGrid />} */}
+					</div>	
+				</div>
+			)	
+	}
 
 export default ProductDetail
